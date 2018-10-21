04db830d4fb1e7c2d4f631ef433693bbbbce01ab
abiwinanda <abiwinanda@outlook.com>
2018-10-21 23:24:12 +0700

    modify train.py to add more parser and support multiple gpu (21 October 2018)

--- train.py
+++ train.py
@@ -9,13 +9,13 @@ from utility.dataset import seperate_dataset_to_labels_folder, create_dataloader
 parser = argparse.ArgumentParser(description='Dr research preprocessing script')
 parser.add_argument('--model', '-m', help='name of the model you want to train')
 parser.add_argument('--classes', '-n', type=int, help='number of output class')
-parser.add_argument('--fe', type=bool, default=True, help='if set to true then train only the new initialized layers')
+parser.add_argument('--feature', '-f', type=bool, default=True, help='if set to true then train only the new initialized layers')
 parser.add_argument('--pretrained', '-p', type=bool, default=True, help='if set to true then use torch pretrained model')
 parser.add_argument('--dataset', '-d', help='path to dataset folder that contain the train and val folder')
 parser.add_argument('--batch', '-b', type=int, default=32, help='batch size')
-parser.add_argument('--gpu', '-g', type=bool, default=True, help='if set to true then train with gpu if exist')
+parser.add_argument('--gpu', '-g', type=int, default=1, help='number of gpu to train')
 parser.add_argument('--epoch', '-e', type=int, default=10, help='number of epoch')
-parser.add_argument('--output', '-o', default='./', help='path save ')
+parser.add_argument('--output', '-o', default='./', help='path to save the trained weights')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -30,10 +30,11 @@ if __name__ == '__main__':
         net = initialize_model(args.model, args.classes, args.fe, args.pretrained)
 
         # send net to gpu
-        if args.gpu:
+        if args.gpu != 0:
             device = torch.device('cuda:0' if torch.cuda.is_avalable() else 'cpu')
         else:
             device = torch.device('cpu')
+        net = nn.DataParallel(net)
         net = net.to(device)
 
         # set the loss function
@@ -44,7 +45,7 @@ if __name__ == '__main__':
         lr_algo = optim.Adam(params_to_update, lr=0.001)
 
         # create the data loader
-        dataloaders = create_dataloader('dataset', 32, 1, True)
+        dataloaders = create_dataloader(args.dataset, args.batch, args.gpu, True)
 
         # train the model
         net, val_acc_hist = train_model(net, dataloaders, loss_func, lr_algo, device, args.epoch)

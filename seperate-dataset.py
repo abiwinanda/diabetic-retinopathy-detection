ff8ca8493507147d6fb0ae586e922dbd17011a1b
abiwinanda <abiwinanda@outlook.com>
2018-10-22 23:26:01 +0700

    add additional argument to seperate_dataset_to_labels_folder function (22 October 2018)

--- seperate-dataset.py
+++ seperate-dataset.py
@@ -5,7 +5,8 @@ parser = argparse.ArgumentParser(description='script to seperate dataset to thei
 parser.add_argument('--src', '-s', help='path to images that want to be seperated')
 parser.add_argument('--dst', '-d', help='location to create the train and val folder')
 parser.add_argument('--csv', '-c', help='path to the csv file that contain the labels')
-parser.add_argument('--split', type=float, help='train test split ratio')
+parser.add_argument('--eff', '-e', type=float, default=1.0, help='if set to 1 then all images in src will be used or seperated')
+parser.add_argument('--split', type=float, default=0.8, help='train test split ratio')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -21,12 +22,7 @@ if __name__ == '__main__':
             'DR': [1,2,3,4]
         }
 
-        if args.split != None:
-            result = seperate_dataset_to_labels_folder(args.src, args.dst, args.csv, label_rule, args.split)
-        else:
-            result = seperate_dataset_to_labels_folder(args.src, args.dst, args.csv, label_rule)
-
-        if result:
+        if seperate_dataset_to_labels_folder(args.src, args.dst, args.csv, label_rule, args.eff, args.split):
             print('seperating dataset successed')
         else:
             print('seperating dataset failed')

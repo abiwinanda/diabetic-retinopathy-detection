67ada4b7ed1cf074ce39f716e2c8e04c5fe68638
abiwinanda <abiwinanda@outlook.com>
2018-12-02 14:28:33 +0700

    add json file to store list of images to be preprocessed (2 December 2018)

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,10 @@
 import argparse
+import json
 from utility.preprocessing import preprocess_images
 
 parser = argparse.ArgumentParser(description='preprocessing script')
 parser.add_argument('--src', '-s', help='path to images that want to be preprocessed')
+parser.add_argument('--partial', '-p', help='path to json file that contain list of images to be preprocessed')
 parser.add_argument('--dst', '-d', help='location to put the preprocess images')
 parser.add_argument('--centre_crop', '-c', action='store_true', help='if set then preprocessing will perform centre crop (default false)')
 parser.add_argument('--format', '-f', type=int, default=0, help='format of output image (0: tiff, else: jpeg)')
@@ -15,7 +17,16 @@ if __name__ == '__main__':
     elif args.dst == None:
         print('Please specify the path to put the preprocessed images using -d flag')
     else:
-        if preprocess_images(args.src, args.dst, args.centre_crop, args.format, args.output):
+        # parse the partial list from the partial json file
+        if (args.partial == None):
+            partial_list = None
+        else:
+            selected_preprocess_images = open(args.partial)
+            selected_preprocess_images = selected_preprocess_images.read()
+            selected_preprocess_images = json.loads(selected_preprocess_images)
+            partial_list = selected_preprocess_images['partial']
+
+        if preprocess_images(args.src, partial_list, args.dst, args.centre_crop, args.format, args.output):
             print('preprocessing successed')
         else:
             print('preprocessing failed')
